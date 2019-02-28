50dacf982596ba00c235ae375d9734285206c3ed
Ivan Kosik <ivankosik91@gmail.com>
2019-03-01 02:34:20 +0300

    Small changes in plugin installation system.

--- image_vision/core/plugin.py
+++ image_vision/core/plugin.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import QObject, pyqtSignal
 
+from inspect import signature
+
 
 class Plugin(QObject):
     installed = pyqtSignal()
@@ -16,10 +18,23 @@ class Plugin(QObject):
     def _install(self):
         pass
 
+    def remove(self):
+        pass
+
+    def _remove(self):
+        pass
+
     @classmethod
-    def required_plugins(cls):
-        import inspect
-        return inspect.getfullargspec(cls.__init__)
+    def required_plugin_classes(cls):
+        return cls.__init__.__annotations__.values()
+
+    @classmethod
+    def init_parameters_number(cls) -> int:
+        return len(signature(cls.__init__).parameters)
+
+    @classmethod
+    def init_annotated(cls) -> bool:
+        return len(cls.required_plugin_classes()) == cls.init_parameters_number() - 1  # minus 1 to ignore |self|
 
     @classmethod
     def name(cls):
--- image_vision/core/plugin_manager.py
+++ image_vision/core/plugin_manager.py
@@ -1,6 +1,6 @@
 from .plugin import Plugin
 
-from typing import Type, List, get_type_hints
+from typing import Type, List
 
 
 class PluginManager:
@@ -23,62 +23,26 @@ class PluginManager:
         if plugin_cls.name() in self.plugins:
             return self.plugins[plugin_cls.name()]
 
-        print('before install required', plugin_cls)
+        assert plugin_cls.init_annotated(), 'Add annotations for plugin {}'.format(plugin_cls)
 
-        print('PLUGIN Req:', plugin_cls.required_plugins())
-
-        import inspect
-        signature = inspect.signature(plugin_cls.__init__)
-        print('INSpect:', signature.parameters)
-        fullargs = inspect.getfullargspec(plugin_cls.__init__)
-        print('full', fullargs)
-        print('args', fullargs.args)
-        print('annot', fullargs.annotations)
-
-        assert len(fullargs.annotations) == len(fullargs.args) - 1, 'Add annotations for plugin {}'.format(plugin_cls)  # minus 1 for |self| parameter
-
-        '''
-        
-        print('INIT TYPES', plugin_cls.__init__.__annotations__)
-        '''
-
+        # Install all required plugins
         required_plugins = []
-
-        # Install all required plugins
-        # print('get_type_hints', get_type_hints(plugin_cls.__init__))
-        annotations = plugin_cls.__init__.__annotations__
-        # issubclass()
-
-        for par_name, par_annotation in plugin_cls.__init__.__annotations__.items():
-            print('CLS:', par_annotation)
-            required_plugin = self.install_plugin(par_annotation)
-            required_plugins.append(required_plugin)
-        '''
-        # Install all required plugins
-        for required_plugin_cls in plugin_cls.required_plugin_classes:
-            print('CLS:', required_plugin_cls)
+        for required_plugin_cls in plugin_cls.required_plugin_classes():
             required_plugin = self.install_plugin(required_plugin_cls)
             required_plugins.append(required_plugin)
-        '''
 
-        print('before Create plugin instance', plugin_cls)
-        # Create plugin instance
+        # Create plugin instance with required plugins as parameters
         plugin = plugin_cls(*required_plugins)
-        print('after Create plugin instance', plugin_cls)
-
-        '''
-        # Install all required plugins
-        for required_plugin_cls in plugin.required_plugin_classes():
-            required_plugin = self.install_plugin(required_plugin_cls)
-
-            # Remove plugin if any required plugin was removed
-            # dep_plugin.removed.connect(plugin.remove)
-        '''
 
         self.plugins[plugin.name()] = plugin
-        print('befor install', plugin)
         plugin.install()
-        print('return plugin', plugin)
+
+        # Remove plugin if any required plugin was removed
+        '''
+        for required_plugin in required_plugins:
+            required_plugin.removed.connect(self.remove_plugin)              ####
+        '''
+
         return plugin
 
     def install_plugins(self, plugin_classes: List[Type[Plugin]]):
